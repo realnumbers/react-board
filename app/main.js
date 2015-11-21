9667aeb5b82da953f0a8fdd7e9299b6a99aaccb9
Julian Sparber <julian@sparber.net>
2015-11-21 12:17:52 +0100

    [fix] removed deprect render function

--- app/main.js
+++ app/main.js
@@ -3,5 +3,4 @@ var React = require('react');
 var View = require("./view.jsx");
 
 //request.loadBusstopsList();
-React.renderComponent(View(), document.getElementById('content'));
-//React.render(<View />, document.body);
+React.render(<View />, document.body);
