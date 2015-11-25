af010bae4ef63f2cdcfdfb6dfe83a737a1cae489
Julian Sparber <julian@sparber.net>
2015-11-25 01:56:42 +0100

    [dev] moved to babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,70 @@
+'use strict';
+
+var del = require('del');
 var gulp = require('gulp');
-var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
 var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
 var connect = require('gulp-connect');
 var watch = require('gulp-watch');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var concat = require('gulp-concat');
-var source = require("vinyl-source-stream");
 
-gulp.task('browserify', function() {
-  var bundler = browserify({
-    entries: ['./app/main.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // We want to convert JSX to normal javascript
-    debug: true, // Gives us sourcemapping
-    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+var paths = {
+  src: './app',
+  des: './build'
+};
+
+
+gulp.task('clean', function(done) {
+  del([paths.des], done);
+});
+
+gulp.task('watch-and-bundle', function () {
+  var b = watchify(browserify('./app/main.js'));
+
+  var bundle = function(){
+    console.log('bundle..');
+    b.transform("babelify", {presets: ["react"]})
+      .bundle()
+      .pipe(source('bundle.js'))
+      //.pipe(buffer())
+      //.pipe(uglify())
+      .pipe(gulp.dest(paths.des));
+  };
+
+  b.on('update', bundle);
+  return bundle();
+});
+
+//command line: browserify -t babelify main.js -o bundle.js
+//browserify -t [ babelify --presets [ react ] ] app/main.js -o bundle.js
+gulp.task('bundle', function () {
+  browserify('./app/main.js')
+    .transform("babelify", {presets: ["react"]})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.des));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '',
+    livereload: true
   });
-  var watcher  = watchify(bundler);
+});
 
-  return watcher
-    .on('update', function () { // When any files update
-      var updateStart = Date.now();
-      console.log('Updating!');
-      watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('main.js'))
-        // This is where you add uglifying etc.
-        .pipe(gulp.dest('./build/'));
-      console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-  .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./build/'));
+gulp.task('html', function () {
+  gulp.src('./index.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./index.html', './build/bundle.js'], ['html']);
 });
 
 
-// I added this so that you see how to run two watch tasks
-gulp.task('css', function () {
-  gulp.watch('styles/**/*.css', function () {
-                         return gulp.src('styles/**/*.css')
-                                                     .pipe(concat('main.css'))
-                                                     .pipe(gulp.dest('build/'));
-                                                     });
-                                                     });
-
-                                                     gulp.task('webserver', function() {
-                                                     connect.server({
-                                                     livereload: true,
-                                                     root: ['.', 'build']
-                                                     });
-                                                     });
-
-                                                     gulp.task('livereload', function() {
-                                                     gulp.src(['./index.html', './build/*.js'])
-                                                     .pipe(watch('./index.html'))
-                                                     .pipe(watch('./build/main.js'))
-                                                     .pipe(connect.reload());
-                                                     });
-
-// Just running the two tasks
-gulp.task('default', ['browserify', 'css', 'webserver', 'livereload']);
+gulp.task('default', ['clean', 'watch-and-bundle', 'connect', 'watch']);
